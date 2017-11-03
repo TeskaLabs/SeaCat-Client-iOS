e73ffc1e62c1c127ed59daf01e4d910a16d482b6
Ales Teska <ales.teska@teskalabs.com>
2017-11-03 02:05:06 +0100

    Add a whole bunch of TODOs.

--- src/SeaCatPlugin.m
+++ src/SeaCatPlugin.m
@@ -39,7 +39,46 @@ static NSMutableArray * SeaCatPlugins = nil;
         [characteristics addEntriesFromDictionary:pchrs];
     }
     
+//TODO:
+//    "pln": sys.implementation.name,
+//    "pli": sys.api_version,
+//    "plv": sys.version,
+//    "plb": sys.platform
 
+//    memset(hwS_buf, '\0', sizeof(hwS_buf));
+//    seacatcc_macos_ioreg(hwS_buf, CFSTR("serial-number"));
+//    memset(hwb_buf, '\0', sizeof(hwb_buf));
+//    seacatcc_macos_ioreg(hwb_buf, CFSTR("board-id"));
+
+//    // Add platform characteristics
+//    chrs.add(String.format("%s\037%s", "plv", Build.VERSION.RELEASE));
+//    chrs.add(String.format("%s\037%s", "pls", Build.VERSION.SDK_INT));
+//    chrs.add(String.format("%s\037%s", "pli", Build.VERSION.INCREMENTAL));
+//    chrs.add(String.format("%s\037%s", "plB", Build.BRAND));
+//    chrs.add(String.format("%s\037%s", "plf", Build.FINGERPRINT));
+//    chrs.add(String.format("%s\037%s", "plI", Build.ID));
+//    chrs.add(String.format("%s\037%s", "plm", Build.MANUFACTURER));
+//    chrs.add(String.format("%s\037%s", "plM", Build.MODEL));
+//    chrs.add(String.format("%s\037%s", "plp", Build.PRODUCT));
+//    chrs.add(String.format("%s\037%s", "plt", Build.TAGS));
+//    chrs.add(String.format("%s\037%s", "plT", Build.TYPE));
+//    chrs.add(String.format("%s\037%s", "plU", Secure.getString(context.getContentResolver(), Secure.ANDROID_ID)));
+//
+//    // Add hardware characteristics
+//    chrs.add(String.format("%s\037%s", "hwb", Build.BOARD));
+//    chrs.add(String.format("%s\037%s", "hwd", Build.DEVICE));
+//    chrs.add(String.format("%s\037%s", "hwS", Build.SERIAL));
+//
+//    DisplayMetrics dm = Resources.getSystem().getDisplayMetrics();
+//    chrs.add(String.format("%s\037%sx%s", "dpr", dm.widthPixels, dm.heightPixels));
+//    chrs.add(String.format("%s\037%s", "dpi", dm.densityDpi));
+//    chrs.add(String.format("%s\037%s", "dpden", dm.density));
+//    chrs.add(String.format("%s\037%s", "dpxdpi", dm.xdpi));
+//    chrs.add(String.format("%s\037%s", "dpydpi", dm.ydpi));
+//
+//    PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+//    chrs.add(String.format("%s\037%s", "apN", pInfo.versionName));
+//    chrs.add(String.format("%s\037%s", "apV", pInfo.versionCode));
 
     const char * characteristics_c[[characteristics count]+1];
     int i=0;
