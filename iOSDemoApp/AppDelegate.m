90103cc0bbfeb92a1409745be8957676629f5a63
Ales Teska <ales.teska@teskalabs.com>
2018-06-07 11:41:24 +0200

    Add an example of the user auth.

--- iOSDemoApp/AppDelegate.m
+++ iOSDemoApp/AppDelegate.m
@@ -19,19 +19,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     [SeaCatClient setLogMask:SC_LOG_FLAG_DEBUG_GENERIC];
+    [SeaCatClient setAuthLocalisedReason:@"SeaCat needs your auth!"];
     [SeaCatClient configure];
     
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [SeaCatClient deauth];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
